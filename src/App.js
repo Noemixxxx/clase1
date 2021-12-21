e05cbba0a14b2b6c2492a0e11f3930170ffdce40
JesicaRodriguez <yesicanoemirodriguez56@gmail.com>
2021-12-20 23:50:07 -0300

    se mando la tarea

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import { headerApp } from './components/HeaderApp';
+import { LisProducts } from './components/LisProducts';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <container>
+<div className="App">
+   <headerApp />
+   <LisProducts />
     </div>
+    </container>
+    
   );
 }
 
---
+++ src/components/HeaderApp.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Row, col } from "react-bootstrap";
+
+export const headerApp =() =>(
+    <Row>
+    <col xs={12}>
+    <div>Tienda de Ropa</div> 
+    </col>
+  </Row>
+);
